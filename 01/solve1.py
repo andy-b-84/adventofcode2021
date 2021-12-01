5d8faec5e0a023738dce6cccf4d519908f07dd66
Patrick Laxton (Peaks) <patrick-externe.laxton@enedis.fr>
2021-12-01 10:41:04 +0100

    feat(aoc): add solution for 01-1

---
+++ 01/solve1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+increases = 0
+
+with open('input1.txt') as input:
+    depthText = input.readline()
+    oldDepth = int(depthText)
+    depthText = input.readline()
+    depth = int(depthText)
+    while depthText:
+        depth = int(depthText)
+        increases += int(depth>oldDepth)
+        oldDepth = depth
+        depthText = input.readline() 
+
+print(increases)
